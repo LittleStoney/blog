e1048bb33cf34a5ed5d725d970b119a9eb0793c4
shixtao <shixtao@qq.com>
2020-01-07 15:37:29 +0800

    服务器搭建完成，展示静态页面

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const app = require('express')()
+const express = require('express'),
+    app = express(),
+    ejs = require('ejs')
+
+app.set('views','./views')
+app.set('view engine','ejs')
+app.engine('html',ejs.__express)
+app.use('/public',express.static(__dirname+'/public'))
 app.get('/',(req,res,next) => {
-    res.send('hello')
+    res.render('home/index.html')
 })
 app.listen(8080,() => {
     console.log('http running at http://localhost:8080')
