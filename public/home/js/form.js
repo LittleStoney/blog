2ca49cba4e07c2077b56ea9ce807826a826c7422
LittleStoney <1085813343@qq.com>
2020-03-28 13:10:37 +0800

    优化logo效果

--- public/home/js/form.js
+++ public/home/js/form.js
@@ -1,8 +1,8 @@
 "use strict";
 // 去除空格
 var inputs = document.querySelectorAll('input[type=text]')
-inputs.forEach(function(item) {
-    item.onkeyup = function(){
+inputs.forEach(function (item) {
+    item.onkeyup = function () {
         this.value = this.value.trim()
     }
     item = null
@@ -14,46 +14,48 @@ var index = addr.lastIndexOf("\/")
 var id = decodeURI(addr.substring(index + 1, addr.length))
 //保存用户名
 var username = localStorage.getItem('username')
-if(username) {
-	$('#name').val(username)
-}else{
-	$('#name').val('')
+if (username) {
+    $('#name').val(username)
+} else {
+    $('#name').val('')
 }
 //提交评论
-$(function(){
-    $('#form').on('submit',function(){
+$(function () {
+    $('#form').on('submit', function () {
         var comment = $('#form-control').val()
         var name = $('input[name=name]').val()
         comment = filterXSS(comment)
         name = filterXSS(name)
         var time = new Date().toLocaleString()
         var face = $('select[name=face]').val()
-        if(!comment.replace(/ +/g,"").replace(/[\r\n]/g,"")){
+        if (!comment.replace(/ +/g, "").replace(/[\r\n]/g, "")) {
             alert('请输入内容！')
             return false
         }
         $.ajax({
-            type:'POST',
-            url:'/blogs/' + id,
-            data:{
-              name:name,
-              comment:comment,
-              time:time,
-              face:face
+            type: 'POST',
+            url: '/blogs/' + id,
+            data: {
+                name: name,
+                comment: comment,
+                time: time,
+                face: face
             },
-			success:function(data){
-                if(data === 'ok'){
-                localStorage.setItem('username',name)
-                $('.table tbody').prepend("<tr><td><div class=\"touxiang\"><img src=\"".concat(face, "\" alt=\"\u4EBA\u7269\u5934\u50CF\" width=\"50\" height=\"50\"><div>").concat(name, "</div></div><div class=\"content\">").concat(comment, "<br><small>").concat(time, "</small></div></td></tr>"))
-                $('#form-control').val('')
-                $('input[name=name]').val('')
-                }else{
+            success: function (data) {
+                if (data === 'ok') {
+                    localStorage.setItem('username', name)
+                    var content = "<tr><td><div class=\"touxiang\"><img src=\"".concat(face, "\" alt=\"\u4EBA\u7269\u5934\u50CF\" width=\"50\" height=\"50\"><div>").concat(name, "</div></div><div class=\"content\">").concat(comment, "<br><small>").concat(time, "</small></div></td></tr>")
+                    $('.table tbody').prepend(content)
+                    $(content).slideDown()
+                    $('#form-control').val('')
+                    $('input[name=name]').val('')
+                } else {
                     alert('非法错误！')
                     return false
-                }  
+                }
             },
-            error:function(error){
-                alert('非法错误！'+error.toString())
+            error: function (error) {
+                alert('非法错误！' + error.toString())
                 return false
             }
         })
@@ -61,41 +63,41 @@ $(function(){
     })
 })
 //显示回复框
-function reply(user_id){
-    $('.table form').css('display','none')
+function reply(user_id) {
+    $('.table form').css('display', 'none')
     $('#' + user_id).show()
-//提交回复
-    $('#' + user_id).on('submit',function(){
+    //提交回复
+    $('#' + user_id).on('submit', function () {
         var replycomment = $('#' + user_id + 'reply').val()
         var replyname = $('#' + user_id + 'name').val()
         var replytime = new Date().toLocaleString()
         var replyface = $('#' + user_id + 'replyface').val()
         replycomment = filterXSS(replycomment)
         replyname = filterXSS(replyname)
-        if(!replycomment.replace(/ +/g,"").replace(/[\r\n]/g,"")){
+        if (!replycomment.replace(/ +/g, "").replace(/[\r\n]/g, "")) {
             alert('请输入内容！')
             return false
         }
         $.ajax({
-            type:'POST',
-            url:'/reply?id=' + user_id + '&blog_id=' + id,
-            data:{
-                replyname:replyname,
-                replycomment:replycomment,
-                replytime:replytime,
-                replyface:replyface
+            type: 'POST',
+            url: '/reply?id=' + user_id + '&blog_id=' + id,
+            data: {
+                replyname: replyname,
+                replycomment: replycomment,
+                replytime: replytime,
+                replyface: replyface
             },
-            success:function(data){
+            success: function (data) {
                 var toname = $('#' + user_id + 'toname').html()
-                if(data === 'ok') {
+                if (data === 'ok') {
                     $('#' + user_id).parent().prepend("<div class=\"reply_area\"><span class=\"reply_info\"><img src=\"".concat(replyface, "\" alt=\"\u4EBA\u7269\u5934\u50CF\" width=\"50\" height=\"50\"> <span>").concat(replyname, "<span class=\"text-info\">&nbsp;\u56DE\u590D&nbsp;</span><span id=\"\"></span>").concat(toname, ":</span></span><span class=\"reply_content\">").concat(replycomment, "<br><small>").concat(replytime, "</small></span></div>"))
                     $('#' + user_id + 'reply').val('')
                     $('#' + user_id + 'name').val('')
                     $('#' + user_id).hide()
                 }
             },
-            error:function(error){
-                alert('非法错误！'+error.toString())
+            error: function (error) {
+                alert('非法错误！' + error.toString())
                 return false
             }
         })
--- public/home/js/style.js
+++ public/home/js/style.js
@@ -1,81 +1,81 @@
-$(function(){
-	 //点击切换颜色按钮
-	 $('#config-trigger').click(function(e){	 	
-	 	e.preventDefault()	 		 	
-	 	if($(this).hasClass('config-panel-open')){
-	       $("#config-panel").animate({
-	         right: "-=190" 
-	        }, 500)
-	       $(this).removeClass('config-panel-open').addClass('config-panel-hide')
-	      }
-	      else {      
-	      $("#config-panel").animate({
-	        right: "+=190"
-	        }, 500);
-	       $(this).removeClass('config-panel-hide').addClass('config-panel-open')  
-	      }
-	 })	 
-	 $('#config-close').on('click', function(e) {
-	     e.preventDefault()
-	     $('#config-trigger').click()
-	 })
-	 //点击选择颜色
-	 $('.theme-1 a').on('click', function() {
-	 	var $listItem = $(this).closest('li')
-	 	$listItem.addClass('active')
-	 	$listItem.siblings().removeClass('active') 	
-	 	$('.header').css('backgroundColor','#5ECCA9')
-		$('.nav-link').css('backgroundColor','#5ECCA9')
-		$('.more-link').css('color','#5ECCA9')
-		$('.list-inline-item a').css('color','#5ECCA9')
-	 	$('.btn-primary').css({
-			'backgroundColor':'#5ECCA9',
-			'borderColor':'#5ECCA9'
-		})
-		localStorage.setItem('color','#5ECCA9')
-	 })
-	 $('.theme-2 a').on('click', function() { 		
-	 	var $listItem = $(this).closest('li')
-	 	$listItem.addClass('active')
-	 	$listItem.siblings().removeClass('active') 	
-		$('.header').css('backgroundColor','#3B7EEB')
-		$('.nav-link').css('backgroundColor','#3B7EEB')
-		$('.more-link').css('color','#3B7EEB')
-		$('.list-inline-item a').css('color','#3B7EEB')
+$(function () {
+	//点击切换颜色按钮
+	$('#config-trigger').click(function (e) {
+		e.preventDefault()
+		if ($(this).hasClass('config-panel-open')) {
+			$("#config-panel").animate({
+				right: "-=190"
+			}, 500)
+			$(this).removeClass('config-panel-open').addClass('config-panel-hide')
+		}
+		else {
+			$("#config-panel").animate({
+				right: "+=190"
+			}, 500);
+			$(this).removeClass('config-panel-hide').addClass('config-panel-open')
+		}
+	})
+	$('#config-close').on('click', function (e) {
+		e.preventDefault()
+		$('#config-trigger').click()
+	})
+	//点击选择颜色
+	$('.theme-1 a').on('click', function () {
+		var $listItem = $(this).closest('li')
+		$listItem.addClass('active')
+		$listItem.siblings().removeClass('active')
+		$('.header').css('backgroundColor', '#5ECCA9')
+		$('.nav-link').css('backgroundColor', '#5ECCA9')
+		$('.more-link').css('color', '#5ECCA9')
+		$('.list-inline-item a').css('color', '#5ECCA9')
 		$('.btn-primary').css({
-			'backgroundColor':'#3B7EEB',
-			'borderColor':'#3B7EEB'
+			'backgroundColor': '#5ECCA9',
+			'borderColor': '#5ECCA9'
 		})
-		localStorage.setItem('color','#3B7EEB')
-	 })
-	 $('.theme-3 a').on('click', function() {
-	 	var $listItem = $(this).closest('li')
-	 	$listItem.addClass('active')
-	 	$listItem.siblings().removeClass('active') 	
-	 	$('.header').css('backgroundColor','#EEA73B')
-		$('.nav-link').css('backgroundColor','#EEA73B')
-		$('.more-link').css('color','#EEA73B')
-		$('.list-inline-item a').css('color','#EEA73B')
-	 	$('.btn-primary').css({
-			'backgroundColor':'#EEA73B',
-			'borderColor':'#EEA73B'
-		})	
-		localStorage.setItem('color','#EEA73B')
-	 })
-	 $('.theme-4 a').on('click', function() {
-	 	var $listItem = $(this).closest('li')
-	 	$listItem.addClass('active')
-	 	$listItem.siblings().removeClass('active') 	
-	 	$('.header').css('backgroundColor','#6C51A4')
-		$('.nav-link').css('backgroundColor','#6C51A4')
-		$('.more-link').css('color','#6C51A4')
-		$('.list-inline-item a').css('color','#6C51A4')
-	 	$('.btn-primary').css({
-			'backgroundColor':'#6C51A4',
-			'borderColor':'#6C51A4'
-		})	
-		localStorage.setItem('color','#6C51A4')
-	 })
+		localStorage.setItem('color', '#5ECCA9')
+	})
+	$('.theme-2 a').on('click', function () {
+		var $listItem = $(this).closest('li')
+		$listItem.addClass('active')
+		$listItem.siblings().removeClass('active')
+		$('.header').css('backgroundColor', '#3B7EEB')
+		$('.nav-link').css('backgroundColor', '#3B7EEB')
+		$('.more-link').css('color', '#3B7EEB')
+		$('.list-inline-item a').css('color', '#3B7EEB')
+		$('.btn-primary').css({
+			'backgroundColor': '#3B7EEB',
+			'borderColor': '#3B7EEB'
+		})
+		localStorage.setItem('color', '#3B7EEB')
+	})
+	$('.theme-3 a').on('click', function () {
+		var $listItem = $(this).closest('li')
+		$listItem.addClass('active')
+		$listItem.siblings().removeClass('active')
+		$('.header').css('backgroundColor', '#EEA73B')
+		$('.nav-link').css('backgroundColor', '#EEA73B')
+		$('.more-link').css('color', '#EEA73B')
+		$('.list-inline-item a').css('color', '#EEA73B')
+		$('.btn-primary').css({
+			'backgroundColor': '#EEA73B',
+			'borderColor': '#EEA73B'
+		})
+		localStorage.setItem('color', '#EEA73B')
+	})
+	$('.theme-4 a').on('click', function () {
+		var $listItem = $(this).closest('li')
+		$listItem.addClass('active')
+		$listItem.siblings().removeClass('active')
+		$('.header').css('backgroundColor', '#6C51A4')
+		$('.nav-link').css('backgroundColor', '#6C51A4')
+		$('.more-link').css('color', '#6C51A4')
+		$('.list-inline-item a').css('color', '#6C51A4')
+		$('.btn-primary').css({
+			'backgroundColor': '#6C51A4',
+			'borderColor': '#6C51A4'
+		})
+		localStorage.setItem('color', '#6C51A4')
+	})
 })
-	
+
 
