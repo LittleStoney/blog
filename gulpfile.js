ab532746ad0c250accdcaa972ea3801d5b4b295d
LittleStoney <1085813343@qq.com>
2020-10-10 14:58:51 +0800

    update dev branch

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const gulp = require('gulp'),
+  cleanCSS = require('gulp-clean-css'),
+  rev = require('gulp-rev'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat');
+
+gulp.task('css', function() {
+  return gulp.src('./app/public/home/css/index.css')
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cleanCSS())
+    .pipe(rev())
+    .pipe(gulp.dest('./app/public/home/css'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('rev/css'));
+});
+
+gulp.task('js', function() {
+  return gulp.src([ './app/public/home/js/form.js', './app/public/home/js/style.js' ])
+    .pipe(concat('index.js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(rev())
+    .pipe(uglify())
+    .pipe(gulp.dest('./app/public/home/js'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('rev/js'));
+});
