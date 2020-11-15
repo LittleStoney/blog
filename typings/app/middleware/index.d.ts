ae0fb76eea07629bbabe31abe6e038f2b5ec7c09
shitao <shixtao@qq.com>
2020-11-15 18:12:33 +0800

    ''

--- typings/app/middleware/index.d.ts
+++ typings/app/middleware/index.d.ts
@@ -2,11 +2,13 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
+import ExportHttps = require('../../../app/middleware/https');
 import ExportLogin = require('../../../app/middleware/login');
 import ExportNotFound = require('../../../app/middleware/notFound');
 
 declare module 'egg' {
   interface IMiddleware {
+    https: typeof ExportHttps;
     login: typeof ExportLogin;
     notFound: typeof ExportNotFound;
   }
